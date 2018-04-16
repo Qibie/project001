e9d633b0baee369e77c0dbf3bdceeea71a39e0ce
herewithyou <1274108397@qq.com>
2018-04-16 21:11:43 +0800

    cnn_char

--- train_bilstm_crf_add_word.py
+++ train_bilstm_crf_add_word.py
@@ -1,8 +1,8 @@
 # coding=utf-8
 import numpy as np
 from bilstm_crf_add_word import BiLSTM_CRF
-from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau,\
-                            TensorBoard
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, \
+    TensorBoard
 from keras.optimizers import Adam, Nadam
 import os
 
@@ -13,7 +13,7 @@ word_embedding_mat = np.load('data/word_embedding_matrix.npy')
 # word_embedding_mat = np.random.randn(157142, 200)
 
 X_train = np.load('data/X_train.npy')
-train_add = np.load('data/word_train_add.npy') # add word_embedding
+train_add = np.load('data/word_train_add.npy')  # add word_embedding
 X_dev = np.load('data/X_dev.npy')
 dev_add = np.load('data/word_dev_add.npy')
 y_train = np.load('data/y_train.npy')
@@ -31,7 +31,7 @@ ner_model = BiLSTM_CRF(n_input_char=200, char_embedding_mat=char_embedding_mat,
                        keep_prob=0.7, n_lstm=256, keep_prob_lstm=0.6, n_entity=7,
                        optimizer=adam, batch_size=32, epochs=10,
                        n_filter=128, kernel_size=3)
-cp_folder, cp_file = 'checkpoints', 'bilstm_crf_add_word_weights_best.hdf5'
+cp_folder, cp_file = 'checkpoints', 'bilstm_crf_add_word_weights_best_1.hdf5'
 log_filepath = os.getcwd() + '/logs/bilstm_crf_add_word_summaries'
 
 cb = [ModelCheckpoint(os.path.join(cp_folder, cp_file), monitor='val_loss',
@@ -42,5 +42,5 @@ cb = [ModelCheckpoint(os.path.join(cp_folder, cp_file), monitor='val_loss',
       ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, mode='min',
                         epsilon=1e-4, cooldown=2, min_lr=1e-8)]
 
-ner_model.train3([X_train, train_add], y_train, [X_dev, dev_add], y_dev, cb)
+ner_model.train4([X_train, train_add], y_train, [X_dev, dev_add], y_dev, cb)
 # ner_model.train([X_train,train_add],y_train,[X_dev,dev_add],y_dev,cb)
