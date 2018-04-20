57261204e595cc395705a220e7a972062170c7ae
herewithyou <1274108397@qq.com>
2018-04-20 19:13:43 +0800

    fsdfs

--- train_bilstm_crf_add_word.py
+++ train_bilstm_crf_add_word.py
@@ -49,5 +49,5 @@ cb = [ModelCheckpoint(os.path.join(cp_folder, cp_file), monitor='val_loss',
       ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, mode='min',
                         epsilon=1e-4, cooldown=2, min_lr=1e-8)]
 # ner_model.train2([X_train, train_add], y_train, [X_dev, dev_add], y_dev, cb)
-ner_model.train_simple([X_train,train_add],y_train,[X_dev,dev_add],y_dev,cb)
-print(ner_model.train_simple.evaluate([X_test,test_add],y_test))
+ner_model.train_simple([train_add],y_train,[dev_add],y_dev,cb)
+print(ner_model.train_simple.evaluate([test_add],y_test))
